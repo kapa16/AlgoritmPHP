466e7e7e72e7f9217697129b6353fb1428e139fb
kapa16 <kapa16@inbox.ru>
2019-03-30 23:23:56 +0500

    task3

--- Lesson1/task3.php
+++ Lesson1/task3.php
@@ -1,2 +1,25 @@
 <?php
 
+
+$queue = new SplQueue();
+
+?>
+
+<form action="" method="post" id="form">
+    <input type="text" name="message" id="">
+    <button type="submit">SEND</button>
+</form>
+
+<script>
+    document.querySelector('#form').addEventListener('submit', (evt) => {
+      evt.preventDefault();
+      console.log(evt);
+      fetch('/task3.php', {
+        method: 'post',
+        mode: "cors",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({"message": evt.target.message.value})
+      })
+        .then(response => response);
+    });
+</script>
