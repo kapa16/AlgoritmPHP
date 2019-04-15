8eac1fbb090db58c53ea2dca307b52cef0391993
Pavel <kapa16@inbox.ru>
2019-04-15 15:15:26 +0500

    Lesson4

--- Lesson4/Parser.php
+++ Lesson4/Parser.php
@@ -3,160 +3,97 @@
 class Parser
 {
     private $config = [
-        "-" => ["priority" => "2", "association" => "left"],
-        "+" => ["priority" => "2", "association" => "left"],
-        "*" => ["priority" => "3", "association" => "left"],
-        "/" => ["priority" => "3", "association" => "left"],
-        "^" => ["priority" => "4", "association" => "right"]
+        '-' => ['priority' => '2', 'association' => 'left'],
+        '+' => ['priority' => '2', 'association' => 'left'],
+        '*' => ['priority' => '3', 'association' => 'left'],
+        '/' => ['priority' => '3', 'association' => 'left'],
+        '^' => ['priority' => '4', 'association' => 'right'],
     ];
     private $stack;
-    private $buffer;
+    private $buffer = [];
 
     public function __construct()
     {
         $this->stack = new SplStack();
-        $this->buffer = [];
     }
 
-    //запустили парсинг
-    public function run($str)
+    public function run($str): array
     {
         $arr = $this->prepareString($str);
         return $this->parse($arr);
     }
 
-    // подготовили строку
     private function prepareString($str)
     {
-        $str = preg_replace("/\s/", "", $str);
-        $str = str_replace(",", ".", $str);
+        $str = preg_replace('/\s/', '', $str);
+        $str = str_replace(',', '.', $str);
         $str = str_split($str);
         return $str;
     }
 
-    //пушим элемент, в соответствии с некоторыми условиями
-    private function pushOperation($value)
+    private function pushOperation($value): void
     {
         while (true) {
             if ($this->stack->isEmpty()) {
-                //если массив пуст, пушим первое значение и выходим из цикла
                 $this->stack->push($value);
                 break;
-            } else {
-                //смотрим какая была последняя операция, извлекаем её
-                $lastOperation = $this->stack->pop();
-                //берём ёё приоритет
-                $prevPriority = $config[$lastOperation]["priority"];
-                //берём нынешнюю операцию её приоритет
-                $currentPriority = $config[$value]["priority"];
-                //соответственно узнаём куда будем добавлять
-                $currentAssociation = $config[$value]["association"];
-                //если налево(все операции кроме степени)
-                if ($currentAssociation === "left") {
-                    //если приоритет текущего действия выше предыдущего
-                    if ($currentPriority > $prevPriority) {
-                        //добавляем в стэк операцию, которую извлекли
-                        $this->stack->push($lastOperation);
-                        //и после неё добавляем нынешнюю и выходим из цикла
-                        $this->stack->push($value);
-                        break;
-                    //если у нас была степень до этого, а сейчас умножение или деление, или сложение, или вычитание, или снова степень
-                    //или если у нас было умножение, а теперь, снова умножение или деление или вычитание или сложение
-                    } else {
-                        //иначе откладываем эту операцию во временное хранилище
-                        //и при новом проходе берём предшевствовавший ему элемент
-                        //сравниваем с нынешним и если его приоритет снова выше
-                        //либо равен текущему, снова бросаем в буфер
-                        $this->buffer[] = $lastOperation;
-                    }
-                //если пушим направо(степень)
-                } elseif ($currentAssociation === "right") {
-                    //если приоритет текущего действия выше предыдущего либо такой же
-                    if ($currentPriority >= $prevPriority) {
-                        //пушим назад предыдущий элемент
-                        $this->stack->push($lastOperation);
-                        //за ним пушим текущий элемент и выходим из цикла
-                        $this->stack->push($value);
-                        break;
-                    } else {
-                        //иначе откладываем эту операцию во временное хранилище
-                        //и при новом проходе берём предшевствовавший ему элемент
-                        //сравниваем с нынешним и если его приоритет снова выше
-                        //снова бросаем в буфер
-                        $this->buffer[] = $lastOperation;
-                    }
-                }
             }
+
+            $lastOperation = $this->stack->pop();
+            $prevPriority = $this->config[$lastOperation]['priority'];
+            $currentPriority = $this->config[$value]['priority'];
+            $currentAssociation = $this->config[$value]['association'];
+
+            if (($currentAssociation === 'left') && $currentPriority > $prevPriority) {
+                $this->stack->push($lastOperation);
+                $this->stack->push($value);
+                break;
+            }
+            if (($currentAssociation === 'right') && $currentPriority >= $prevPriority) {
+                $this->stack->push($lastOperation);
+                $this->stack->push($value);
+                break;
+            }
+            $this->buffer[] = $lastOperation;
         }
     }
 
-    private function parse($arr)
+    private function parse($arr): array
     {
-        //начинаем перебор массива
-        //изначально ожидаем получить число
         $lastSymbolIsNumber = true;
         foreach ($arr as $key => $value) {
-            //сверяем с паттерном "/[\+\-\*\/\^]/", и если подходит
-            //а это значит, что у нас не число
-            if (preg_match("/[\+\-\*\/\^]/", $value)) {
-                //пушим текущую операцию
+            if (preg_match('/[\+\-\*\/\^]/', $value)) {
                 $this->pushOperation($value);
-                //и делаем себе пометку, что у нас была операция
                 $lastSymbolIsNumber = false;
-            //проверяем на число или букву(то есть переменнную)
-            } elseif (preg_match("/[0-9a-zA-Z\.]/", $value)) {
-                //смотрим, а до этого было число или нет и если число
+            }
+            if (preg_match('/[0-9a-zA-Z\.]/', $value)) {
                 if ($lastSymbolIsNumber) {
-                    //тогда нынешнее число добавляем в буфер(не в стэк) склеивая с предыдущим
-                    //смысл в том, что когда мы разбивали наше выражение, числа, которые там могли быть
-                    //тоже разбились, например 234 превратилось в 3 элемента массива: 2, 3, 4
-                    //и здесь мы просто снова собираем это число
                     $this->buffer[] = array_pop($this->buffer) . $value;
-                //если предыдущее было не число
                 } else {
-                    //нынешнее добавляем в буфер
                     $this->buffer[] = $value;
-                    //и запоминаем, что мы добавили именно число
                     $lastSymbolIsNumber = true;
                 }
-            //возможно у нас не число не оператор, а левая скобка
-            } elseif ($value == "(") {
-                //тогда пушим скобку
+            }
+            if ($value === '(') {
                 $this->stack->push($value);
-                //и запоминаем, что это не скобка
                 $lastSymbolIsNumber = false;
-            //если другая скобка, то всё
-            } elseif ($value == ")") {
+            }
+            if ($value === ')') {
                 while (true) {
-                    //достаём последний символ в стопке
                     $symbol = $this->stack->pop();
-                    //и если он не открывающая скобка, кладём в буфер
-                    if ($symbol != "(") {
-                        $this->buffer[] = $symbol;
-                    //а если открывающая скобка, то просто закончили добавлять всё в буфер
-                    //таким образом в буфер попало только выражение из скобок без самих скобок
-                    } else {
+                    if ($symbol === '(') {
                         break;
                     }
+                    $this->buffer[] = $symbol;
                 }
-                //и так как закончили мы за открывающей скобке,
-                //то говорим себе что оно было не числом
                 $lastSymbolIsNumber = false;
             }
         }
 
-        //после того как мы прошлись всем символам
-        //узнаём какой длины оказался стэк
-        //вне стэка(в буфере) у нас всё что было в скобках
-        //а так же всё что было с меньшим приоритетом 
         $length = $this->stack->count();
         for ($i = 0; $i < $length; $i++) {
-            //и теперь сам стэк добавляем в буфер
             $this->buffer[] = $this->stack->pop();
         }
-        //получаем наш отсортированный массив
         return $this->buffer;
     }
 }
-//добавление в буфер дало нам то, что в самом начале
-//теперь раполагаются самые высокоприоритетные операции
--- Lesson4/Tree.php
+++ Lesson4/Tree.php
@@ -8,9 +8,9 @@ class Tree
     private $y;
     private $z;
     private $vars = [
-        "x",
-        "y",
-        "z"
+        'x',
+        'y',
+        'z'
     ];
 
     public function __construct()
@@ -20,85 +20,63 @@ class Tree
 
     public function runCalculation($x = 0, $y = 0, $z = 0)
     {
-        //если нам ввели числа, производим рассчёт
-        if (is_numeric($x) && is_numeric($y) && is_numeric($x)) {
+        if (is_numeric($x) && is_numeric($y) && is_numeric($z)) {
             $this->x = $x;
             $this->y = $y;
             $this->z = $z;
-            //возвращаем расчёт
-            return $this->calculate($this->root);
-        //иначе говорим, что получили не число
-        } else {
-            return "not a number";
+            return $this->calculate(clone $this->root);
         }
+        return 'not a number';
     }
 
     public function buildTree($arr)
     {
-        //получили массив
         foreach ($arr as $item) {
-            //вставляем элементы в массив
             $this->insert($item);
         }
-        //
         $this->root = $this->stack->pop();
         return $this->root;
     }
     
-    private function insert($item)
+    private function insert($item): void
     {
-        //если вставляемый в массив элемент - число или переменная
-        if (preg_match("/[0-9a-zA-Z\.]/", $item)) {
-            //тогда пушим его как новую ноду и даём ей имя, равное переданному элементу
+        if (preg_match('/[0-9a-zA-Z\.]/', $item)) {
             $this->stack->push(new Node($item));
-        //а если это операци
-        } elseif (preg_match("/[\+\-\*\/\^]/", $item)) {
-            //тогда достаём два последних значения
+        } elseif (preg_match('/[\+\-\*\/\^]/', $item)) {
             $leftNode = $this->stack->pop();
             $rightNode = $this->stack->pop();
-            //и создаём новую ноду
-            //этой ноде мы передаём качестве потомков элементы, которые извлекли
             $this->stack->push(new Node($item, $leftNode, $rightNode));
         }
     }
 
-    //рассчитываем значение выражения
-    private function calculate(Node &$node)
+    private function calculate(Node $node)
     {
-        //если у нас число или переменная
-        if (preg_match("/[0-9a-zA-Z\.]/", $node->value)) {
-            //если это всё таки переменная, а не число
-            if (in_array($node->value, $this->vars)) {
-                //получаем значение этой перменной
+        if (preg_match('/[0-9a-zA-Z\.]/', $node->value)) {
+            if (in_array($node->value, $this->vars, true)) {
                 return $this->assignValue($node->value);
             }
-            //а если у нас чило, просто возвращаем его
             return $node->value;
-        //иначе, если у нас операция
-        } elseif (preg_match("/[\+\-\*\/\^]/", $node->value)) {
-            //смотрим какая у нас операция
-            //ну и так как при построении дерева, мы записывали для операций
-            //в качестве наследников переменные, просто получаем их,
-            //и произодим необходимую операцию
+        }
+
+        if (preg_match('/[\+\-\*\/\^]/', $node->value)) {
             switch ($node->value) {
-                case "+": {
+                case '+': {
                     return $this->calculate($node->right) + $this->calculate($node->left);
                 }
-                case "-": {
+                case '-': {
                     return $this->calculate($node->right) - $this->calculate($node->left);
                 }
-                case "*": {
+                case '*': {
                     return $this->calculate($node->right) * $this->calculate($node->left);
                 }
-                case "^": {
-                    return pow($this->calculate($node->right), (int)$this->calculate($node->left));
+                case '^': {
+                    return $this->calculate($node->right) ** (int)$this->calculate($node->left);
                 }
-                case "/": {
+                case '/': {
                     try {
                         return $this->calculate($node->right) / $this->calculate($node->left);
-                    //если делим на ноль, возвращаем ошибку
                     } catch (ArithmeticError $e) {
-                        exit("division by zero" . $e->getTraceAsString());
+                        exit('division by zero' . $e->getTraceAsString());
                     }
                 }
             }
@@ -107,16 +85,15 @@ class Tree
 
     private function assignValue($value)
     {
-        //просто получаем наши переменные, если такие есть
         switch ($value) {
-            case "x":
+            case 'x':
                 return $this->x;
-            case "y":
+            case 'y':
                 return $this->y;
-            case "z":
+            case 'z':
                 return $this->z;
             default:
-                exit("not accepted variable");
+                exit('not accepted variable');
         }
     }
 }
